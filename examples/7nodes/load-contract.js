2844feb8b9d1318dc44b3cf4b8f223c0a3b86984
Szkered <emperorview@gmail.com>
2018-06-06 18:46:19 +0800

    add permission contract into genesis.json

---
+++ examples/7nodes/load-contract.js
@@ -0,0 +1,6 @@
+a = eth.accounts[0]
+web3.eth.defaultAccount = a;
+
+var abi = [{"constant":true,"inputs":[],"name":"storedData","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initVal","type":"uint256"}],"payable":false,"type":"constructor"}];
+
+var simple = web3.eth.contract(abi).at("0x1932c48b2bf8102ba33b4a6b545c32236e342f34")
