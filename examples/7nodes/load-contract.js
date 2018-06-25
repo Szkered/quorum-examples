2011f06e5eb436d899fcb1b377fa5ebc381425b1
Qvintvs <emperorview@gmail.com>
2018-06-25 17:36:15 +0800

    add load-contract script

--- examples/7nodes/load-contract.js
+++ examples/7nodes/load-contract.js
@@ -3,4 +3,4 @@ web3.eth.defaultAccount = a;
 
 var abi = [{"constant":true,"inputs":[],"name":"storedData","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initVal","type":"uint256"}],"payable":false,"type":"constructor"}];
 
-var simple = web3.eth.contract(abi).at("0x1932c48b2bf8102ba33b4a6b545c32236e342f34")
+var simple = web3.eth.contract(abi).at("0x9d13c6d3afe1721beef56b55d303b09e021e27ab")
